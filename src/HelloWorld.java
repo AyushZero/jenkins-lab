a98c5dfb37d695987b2c334488919031d61488f7
AyushZero <ayushsingh30082005@gmail.com>
2026-03-08 16:48:49 +0530

    Webhook Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Jenkins - CI Working!");
+        System.out.println("Automatic CI trigger test");
 
     }
 
