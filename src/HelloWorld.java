abd24e8e817b8d9566fb3e457ef7cc91f26aacbf
AyushZero <ayushsingh30082005@gmail.com>
2026-03-08 16:11:41 +0530

    pom.xml change1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Jenkins CI Pipeline!");
+        System.out.println("Hello Jenkins - CI Working!");
 
     }
 
